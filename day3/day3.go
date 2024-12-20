123bc18f07edf08e78893a8f40038ee041cab427
Siarhei Putsik <siarhey.putsik.sp@lhsystems.com>
2024-12-20 17:30:24 +0100

    day 3

--- day3/day3.go
+++ day3/day3.go
@@ -1,11 +1,14 @@
 package day3
 
-import "fmt"
+import (
+	"aoc/util"
+	"strings"
+)
 
 func Solve(lines []string) int {
 	total := 0
 	for _, line := range lines {
-		solveLine(line)
+		total += solveLine(line)
 	}
 
 	return total
@@ -14,13 +17,35 @@ func Solve(lines []string) int {
 func solveLine(line string) int {
 	prefix := "mul("
 	mem := []rune{}
-	for _, rune := range line {
-		mem = append(mem, rune)
-		fmt.Println(mem)
-		if prefix == string(mem) {
 
+	total := 0
+	for i := 0; i < len(line); i++ {
+		ch := rune(line[i])
+
+		mem = append(mem, ch)
+		if prefix == string(mem) {
+			i, first := getNumber(line, i+1, ',')
+			i, second := getNumber(line, i+1, ')')
+
+			total += first * second
+			mem = []rune{}
+		} else if !strings.HasPrefix(prefix, string(mem)) {
+			mem = []rune{}
 		}
+
 	}
 
-	return 0
+	return total
+}
+
+func getNumber(line string, start int, end rune) (index, number int) {
+	var str []rune
+	for ch := rune(line[start]); ch != end; ch = rune(line[start]) {
+		if start == len(line)-1 {
+			break
+		}
+		str = append(str, ch)
+		start++
+	}
+	return start, util.ParseInt(string(str))
 }
